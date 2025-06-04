43156fea22a31314060da3782c7c689de3778f95
Kelvin <kierulfkelvin@gmail.com>
2025-06-04 21:37:53 +0800

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,15 @@
 import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
   let items = ["Nigga1", "Nigga2", "Nigga3", "Nigga4"];
-  items = [];
-  if (items.length == 0) {
-    return (
-      <>
-        <h1>Hello Bichaess</h1>
-        <p>No Items Found</p>
-      </>
-    );
-  }
-
+  // items = [];
   return (
     <>
-      <h1>Hello Bitchaess</h1>
-      <ul className="list group">
+      <h1>List</h1>
+      {/* {items.length == 0 ? <p>No Item found </p> : null} */}
+      {items.length == 0 && <p>No Item Found</p>}
+      <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li className="list-group-item">{item}</li>
         ))}
       </ul>
     </>
